5d1ab706398a92af8d27f88114cacbd2f234dc73
Richard Beauchamp <richard.beauchamp@tree.com>
2015-11-16 17:40:45 -0800

    Added an OData SwaggerProvider

---
+++ Swashbuckle.OData/ODataSwaggerProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.OData.Edm;
+using Swashbuckle.Swagger;
+
+namespace Swashbuckle.OData
+{
+    public class ODataSwaggerProvider : ISwaggerProvider
+    {
+        private readonly IEdmModel _edmModel;
+
+        public ODataSwaggerProvider(IEdmModel edmModel)
+        {
+            _edmModel = edmModel;
+        }
+
+        public SwaggerDocument GetSwagger(string rootUrl, string apiVersion)
+        {
+            var edmSwaggerDocument = new ODataSwaggerConverter(_edmModel).ConvertToSwaggerModel();
+
+            var rootUri = new Uri(rootUrl);
+            var port = !rootUri.IsDefaultPort ? ":" + rootUri.Port : string.Empty;
+            edmSwaggerDocument.host = rootUri.Host + port;
+            edmSwaggerDocument.basePath = rootUri.AbsolutePath != "/" ? rootUri.AbsolutePath : "/odata";
+            edmSwaggerDocument.schemes = new[] { rootUri.Scheme }.ToList();
+
+            return edmSwaggerDocument;
+        }
+    }
+}
