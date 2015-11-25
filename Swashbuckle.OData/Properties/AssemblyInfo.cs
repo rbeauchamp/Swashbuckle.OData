d86a242e23d1a6df2abfcdc0452a1f5306a7c9b1
Richard Beauchamp <richard.beauchamp@tree.com>
2015-11-25 15:53:38 -0800

    Release v2.0.0

--- Swashbuckle.OData/Properties/AssemblyInfo.cs
+++ Swashbuckle.OData/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-alpha-2")]
+[assembly: AssemblyInformationalVersion("2.0.0")]
