330478cf2e86b479a10a4be4fdc07244b57d26d1
Richard Beauchamp <richard.beauchamp@tree.com>
2015-12-04 18:42:49 -0800

    Remove unneeded unit test

--- Swashbuckle.OData.Tests/Fixtures/PostTests.cs
+++ Swashbuckle.OData.Tests/Fixtures/PostTests.cs
@@ -11,26 +11,6 @@ namespace Swashbuckle.OData.Tests
     public class PostTests
     {
         [Test]
-        public async Task It_has_a_body_content_type_of_application_json()
-        {
-            using (WebApp.Start(TestWebApiStartup.BaseAddress, appBuilder => new TestWebApiStartup().Configuration(appBuilder)))
-            {
-                // Arrange
-                var httpClient = HttpClientUtils.GetHttpClient();
-
-                // Act
-                var swaggerDocument = await httpClient.GetAsync<SwaggerDocument>("swagger/docs/v1");
-
-                // Assert
-                PathItem pathItem;
-                swaggerDocument.paths.TryGetValue("/Customers", out pathItem);
-                pathItem.Should().NotBeNull();
-                pathItem.post.Should().NotBeNull();
-                pathItem.post.consumes.Should().Contain("application/json");
-            }
-        }
-
-        [Test]
         public async Task It_has_a_summary()
         {
             using (WebApp.Start(TestWebApiStartup.BaseAddress, appBuilder => new TestWebApiStartup().Configuration(appBuilder)))
