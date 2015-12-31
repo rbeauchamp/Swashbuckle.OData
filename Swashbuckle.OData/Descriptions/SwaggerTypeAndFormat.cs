0b1e286719e5de4e4a184fc14a1000cde13a2b96
Richard Beauchamp <richard.beauchamp@tree.com>
2015-12-31 11:11:12 -0800

    Remove dead code

--- Swashbuckle.OData/Descriptions/SwaggerTypeAndFormat.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Swashbuckle.OData.Descriptions
-{
-    internal class SwaggerTypeAndFormat
-    {
-        public string Type { get; set; }
-
-        public string Format { get; set; }
-    }
-}
