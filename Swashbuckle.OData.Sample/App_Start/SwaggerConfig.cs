7576a5728f2a509ed3e504d3838173a4643cfdc6
Richard Beauchamp <richard.beauchamp@tree.com>
2015-12-31 06:32:20 -0800

    Update sample API

--- Swashbuckle.OData.Sample/App_Start/SwaggerConfig.cs
+++ Swashbuckle.OData.Sample/App_Start/SwaggerConfig.cs
@@ -34,7 +34,9 @@ namespace SwashbuckleODataSample
                 // hold additional metadata for an API. Version and title are required but you can also provide
                 // additional fields by chaining methods off SingleApiVersion.
                 //
-                c.SingleApiVersion("v1", "Swashbuckle.OData Sample App");
+                c.SingleApiVersion("v1", "Sample API for Swashbuckle.OData")
+                    .Contact(contactBuilder => contactBuilder
+                    .Url("https://github.com/rbeauchamp/Swashbuckle.OData"));
 
                 // If your API has multiple versions, use "MultipleApiVersions" instead of "SingleApiVersion".
                 // In this case, you must provide a lambda that tells Swashbuckle which actions should be
